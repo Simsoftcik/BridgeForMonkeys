49a35aabe5f493be79abfeb8ff52acb70fc66f23
Szymon <szymek.kubiczek@gmailcom>
2025-01-26 14:21:59 +0100

    repo clean

--- components/HeightLight.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HeightLight = ({ children }) => {
-  return <code className={"nextra-code"}>{children}</code>;
-}
-
-export default HeightLight;
--- pages/_meta.js
+++
@@ -1,6 +0,0 @@
-const meta = {
-    "index" : "intro",
-    "lesson1" : "#1 Alfabet brydżowy💥🛡️🏷",
-}
-
-export default meta;
