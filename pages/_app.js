90d92456940d4e7b159c5258b58fad88a4d55087
Szymon <szymek.kubiczek@gmailcom>
2025-02-15 15:43:21 +0100

    lekcja 1

--- pages/_app.js
+++
@@ -1,5 +0,0 @@
-import "../css/embla.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
---
+++ pages/_meta.js
@@ -0,0 +1,6 @@
+const meta = {
+    "index" : "intro",
+    "lesson1" : "#1 Alfabet brydżowy💥🛡️🏷",
+}
+
+export default meta;
